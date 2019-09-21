90c22f87cf70da1c2e92f2f9cdf94bbd01984927
Vykunth Srinivasan <vs@Vykunths-MacBook-Pro.local>
2019-09-21 15:53:52 +0530

    Git Android studio first commit

---
+++ app/src/main/java/com/example/electionfingerprint/MainActivity.java
@@ -0,0 +1,14 @@
+ppppackage com.example.electionfingerprint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
