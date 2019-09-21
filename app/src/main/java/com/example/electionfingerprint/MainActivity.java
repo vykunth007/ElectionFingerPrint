b6c5046c1f3a69294c1cfdb8b75021f0dc784d8d
vykunth007 <vykunth007@gmail.com>
2019-09-21 15:58:11 +0530

    Message Log Added

--- app/src/main/java/com/example/electionfingerprint/MainActivity.java
+++ app/src/main/java/com/example/electionfingerprint/MainActivity.java
@@ -1,8 +1,9 @@
-ppppackage com.example.electionfingerprint;
+package com.example.electionfingerprint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("Tag", "Message");
     }
 }
